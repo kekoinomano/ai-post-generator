c69589d394b0ad43bfeeb3f78e39a039668c4562
kekoinomano <91256469+kekoinomano@users.noreply.github.com>
2022-12-03 11:50:43 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
-if (!function_exists('return_json')){
+if (!function_exists('ai_post_generator_return_json_2')){
     /**
      * return_json
      * 
      * @param array $response
      * @return json
      */
-    function return_json($response = array()) {
+    function ai_post_generator_return_json_2($response = array()) {
         header('Content-Type: application/json');
         exit(json_encode($response));
     }
 }
-if (!function_exists('stripAccents')){
-    function stripAccents($str) {
+if (!function_exists('ai_post_generator_stripAccents2')){
+    function ai_post_generator_stripAccents2($str) {
         return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
     }
 }
